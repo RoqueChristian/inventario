476af56dd7d9fb1a57604da1e833b0aacdc6438f
christianr <roquematiaschristian@gmail.com>
2025-12-22 10:38:21 -0300

    comentando codigos

--- inventario.py
+++ inventario.py
@@ -1,50 +1,50 @@
-import pandas as pd
-import oracledb
-from dotenv import load_dotenv
-import os
+# import pandas as pd
+# import oracledb
+# from dotenv import load_dotenv
+# import os
 
-# Configuração Técnica
-oracledb.init_oracle_client(lib_dir=r"C:\\instantclient_23_9")
-load_dotenv()
+# # Configuração Técnica
+# oracledb.init_oracle_client(lib_dir=r"C:\\instantclient_23_9")
+# load_dotenv()
 
-user = os.getenv('user', '')
-password = os.getenv('password', '')
-dsn = os.getenv('dsn', '192.168.0.1/WINT')
+# user = os.getenv('user', '')
+# password = os.getenv('password', '')
+# dsn = os.getenv('dsn', '192.168.0.1/WINT')
 
-def get_sql(operacao):
-    suffix = 'entrada' if operacao == 'EI' else 'saida'
-    return f"""
-    SELECT
-        M.codfilial, M.numnota AS numero_nota, M.codprod, P.descricao AS nome_produto, 
-        C.CATEGORIA AS categoria, TRUNC(M.dtmov) AS dtmov_{suffix}, 
-        SUM(M.qtcont) AS qtd_{suffix},
-        ROUND(SUM(M.punitcont * M.qtcont), 2) AS vlr_{suffix}
-    FROM pcmov M
-    LEFT JOIN pcprodut P ON P.codprod = M.codprod
-    LEFT JOIN pcdepto D ON D.CODEPTO = P.CODEPTO
-    LEFT JOIN PCSECAO S ON S.CODSEC = P.CODSEC AND S.codepto = D.codepto
-    LEFT JOIN pccategoria C ON C.CODCATEGORIA = P.CODCATEGORIA AND C.codsec = S.codsec
-    WHERE M.codoper = '{operacao}' AND EXTRACT(YEAR FROM M.dtmov) = 2025
-    GROUP BY M.codfilial, M.numnota, M.codprod, P.descricao, C.CATEGORIA, TRUNC(M.dtmov)
-    HAVING SUM(M.qtcont) <> 0
-    """
+# def get_sql(operacao):
+#     suffix = 'entrada' if operacao == 'EI' else 'saida'
+#     return f"""
+#     SELECT
+#         M.codfilial, M.numnota AS numero_nota, M.codprod, P.descricao AS nome_produto, 
+#         C.CATEGORIA AS categoria, TRUNC(M.dtmov) AS dtmov_{suffix}, 
+#         SUM(M.qtcont) AS qtd_{suffix},
+#         ROUND(SUM(M.punitcont * M.qtcont), 2) AS vlr_{suffix}
+#     FROM pcmov M
+#     LEFT JOIN pcprodut P ON P.codprod = M.codprod
+#     LEFT JOIN pcdepto D ON D.CODEPTO = P.CODEPTO
+#     LEFT JOIN PCSECAO S ON S.CODSEC = P.CODSEC AND S.codepto = D.codepto
+#     LEFT JOIN pccategoria C ON C.CODCATEGORIA = P.CODCATEGORIA AND C.codsec = S.codsec
+#     WHERE M.codoper = '{operacao}' AND EXTRACT(YEAR FROM M.dtmov) = 2025
+#     GROUP BY M.codfilial, M.numnota, M.codprod, P.descricao, C.CATEGORIA, TRUNC(M.dtmov)
+#     HAVING SUM(M.qtcont) <> 0
+#     """
 
-sql_estoque = "SELECT codprod, codfilial, SUM(qtest) AS qtd_estoque_atual FROM pcest WHERE qtest > 0 GROUP BY codprod, codfilial"
+# sql_estoque = "SELECT codprod, codfilial, SUM(qtest) AS qtd_estoque_atual FROM pcest WHERE qtest > 0 GROUP BY codprod, codfilial"
 
-try:
-    with oracledb.connect(user=user, password=password, dsn=dsn) as conn:
-        print("Extraindo movimentações...")
-        df_ei = pd.read_sql(get_sql('EI'), con=conn)
-        df_si = pd.read_sql(get_sql('SI'), con=conn)
-        df_est = pd.read_sql(sql_estoque, con=conn)
+# try:
+#     with oracledb.connect(user=user, password=password, dsn=dsn) as conn:
+#         print("Extraindo movimentações...")
+#         df_ei = pd.read_sql(get_sql('EI'), con=conn)
+#         df_si = pd.read_sql(get_sql('SI'), con=conn)
+#         df_est = pd.read_sql(sql_estoque, con=conn)
         
-        # Merge com Estoque Atual
-        df_final_ei = pd.merge(df_ei, df_est, on=['CODFILIAL', 'CODPROD'], how='left')
-        df_final_si = pd.merge(df_si, df_est, on=['CODFILIAL', 'CODPROD'], how='left')
+#         # Merge com Estoque Atual
+#         df_final_ei = pd.merge(df_ei, df_est, on=['CODFILIAL', 'CODPROD'], how='left')
+#         df_final_si = pd.merge(df_si, df_est, on=['CODFILIAL', 'CODPROD'], how='left')
 
-        # Exportação
-        df_final_ei.to_csv('inventario_entrada.csv', index=False, sep=';', encoding='utf-8-sig', decimal=',')
-        df_final_si.to_csv('inventario_saida.csv', index=False, sep=';', encoding='utf-8-sig', decimal=',')
-        print("Arquivos CSV gerados com sucesso! ✅")
-except Exception as e:
-    print(f"Erro no processo de extração: {e}")
+#         # Exportação
+#         df_final_ei.to_csv('inventario_entrada.csv', index=False, sep=';', encoding='utf-8-sig', decimal=',')
+#         df_final_si.to_csv('inventario_saida.csv', index=False, sep=';', encoding='utf-8-sig', decimal=',')
+#         print("Arquivos CSV gerados com sucesso! ✅")
+# except Exception as e:
+#     print(f"Erro no processo de extração: {e}")
--- iventario_finalizar.py
+++ iventario_finalizar.py
@@ -1,85 +1,85 @@
-import pandas as pd
-import oracledb
-from dotenv import load_dotenv
-import os
+# import pandas as pd
+# import oracledb
+# from dotenv import load_dotenv
+# import os
 
 
 
-oracledb.init_oracle_client(lib_dir=r"C:\\instantclient_23_9")
+# oracledb.init_oracle_client(lib_dir=r"C:\\instantclient_23_9")
 
 
-load_dotenv()
-user = os.getenv('user', '')
-password = os.getenv('password', '')
-dsn = '192.168.0.1/WINT'
+# load_dotenv()
+# user = os.getenv('user', '')
+# password = os.getenv('password', '')
+# dsn = '192.168.0.1/WINT'
 
 
-# ----------------------------------------------------
-# Consultas SQL 
-# ----------------------------------------------------
-sql_inventario_pendente_baixa = """
-    SELECT M.CODFILIAL AS FILIAL
-        , M.NUMNOTA AS INVENTARIO
-        , TO_CHAR(M.DTMOV, 'DD/MM/YYYY') AS DATA_ATUALIZACAO
-        , M.CODFUNCLANC AS CODIGO_USUARIO
-        , (SELECT NOME FROM PCEMPR WHERE MATRICULA = M.CODFUNCLANC AND ROWNUM = 1) AS NM_USUARIO_ATUALIZACAO
-        , MAX(NVL(M.NUMTRANSENT, 0)) AS TRANSACAO_ENT
-        , MAX(NVL(M.NUMTRANSVENDA, 0)) AS TRANSACAO_SAIDA
-        -- Aplicamos o SUM para totalizar os itens da nota no agrupamento
-        , ROUND(SUM((NVL(M.PUNIT, 0) + NVL(M.VLOUTRASDESP, 0) + NVL(M.VLFRETE_RATEIO, 0) +
-                NVL(M.VLOUTROS, 0) + NVL(M.VLIPI, 0) + NVL(M.ST, 0) - NVL(M.VLREPASSE, 0)) * NVL(M.QT, 0)), 2) 
-        AS VALOR
-    FROM PCMOV M
-    WHERE M.NUMNOTA IS NOT NULL
-    AND M.CODOPER IN ('EI', 'SI')
-    AND M.STATUS = 'B'
-    AND NVL(M.QT, 0) > 0
-    AND M.DTCANCEL IS NULL
-    AND M.DTMOV BETWEEN TO_DATE('2025-10-01', 'YYYY-MM-DD') AND TO_DATE('2025-12-17', 'YYYY-MM-DD')
-    --AND M.CODFILIAL = '5'
-    AND NOT EXISTS (SELECT 1
-                        FROM PCMOVCOMPLE MC
-                        JOIN PCMOV P2 ON P2.NUMTRANSITEM = MC.NUMTRANSITEM
-                        WHERE MC.NUMINVENT = M.NUMNOTA
-                        --AND P2.CODFILIAL = '5'
-                        AND P2.CODOPER IN ('EI', 'SI', 'EA', 'SA')
-                        AND P2.STATUS = 'A'
-                        AND P2.DTMOV >= TO_DATE('2025-10-01', 'YYYY-MM-DD')
-                        AND P2.CODPROD = M.CODPROD
-                        AND P2.DTCANCEL IS NULL)
-    GROUP BY M.CODFILIAL
-        , M.NUMNOTA
-        , M.STATUS
-        , M.DTMOV 
-        , M.CODFUNCLANC
-    ORDER BY M.CODFILIAL
-        , M.DTMOV DESC
-        , M.NUMNOTA DESC
-"""
+# # ----------------------------------------------------
+# # Consultas SQL 
+# # ----------------------------------------------------
+# sql_inventario_pendente_baixa = """
+#     SELECT M.CODFILIAL AS FILIAL
+#         , M.NUMNOTA AS INVENTARIO
+#         , TO_CHAR(M.DTMOV, 'DD/MM/YYYY') AS DATA_ATUALIZACAO
+#         , M.CODFUNCLANC AS CODIGO_USUARIO
+#         , (SELECT NOME FROM PCEMPR WHERE MATRICULA = M.CODFUNCLANC AND ROWNUM = 1) AS NM_USUARIO_ATUALIZACAO
+#         , MAX(NVL(M.NUMTRANSENT, 0)) AS TRANSACAO_ENT
+#         , MAX(NVL(M.NUMTRANSVENDA, 0)) AS TRANSACAO_SAIDA
+#         -- Aplicamos o SUM para totalizar os itens da nota no agrupamento
+#         , ROUND(SUM((NVL(M.PUNIT, 0) + NVL(M.VLOUTRASDESP, 0) + NVL(M.VLFRETE_RATEIO, 0) +
+#                 NVL(M.VLOUTROS, 0) + NVL(M.VLIPI, 0) + NVL(M.ST, 0) - NVL(M.VLREPASSE, 0)) * NVL(M.QT, 0)), 2) 
+#         AS VALOR
+#     FROM PCMOV M
+#     WHERE M.NUMNOTA IS NOT NULL
+#     AND M.CODOPER IN ('EI', 'SI')
+#     AND M.STATUS = 'B'
+#     AND NVL(M.QT, 0) > 0
+#     AND M.DTCANCEL IS NULL
+#     AND M.DTMOV BETWEEN TO_DATE('2025-10-01', 'YYYY-MM-DD') AND TO_DATE('2025-12-17', 'YYYY-MM-DD')
+#     --AND M.CODFILIAL = '5'
+#     AND NOT EXISTS (SELECT 1
+#                         FROM PCMOVCOMPLE MC
+#                         JOIN PCMOV P2 ON P2.NUMTRANSITEM = MC.NUMTRANSITEM
+#                         WHERE MC.NUMINVENT = M.NUMNOTA
+#                         --AND P2.CODFILIAL = '5'
+#                         AND P2.CODOPER IN ('EI', 'SI', 'EA', 'SA')
+#                         AND P2.STATUS = 'A'
+#                         AND P2.DTMOV >= TO_DATE('2025-10-01', 'YYYY-MM-DD')
+#                         AND P2.CODPROD = M.CODPROD
+#                         AND P2.DTCANCEL IS NULL)
+#     GROUP BY M.CODFILIAL
+#         , M.NUMNOTA
+#         , M.STATUS
+#         , M.DTMOV 
+#         , M.CODFUNCLANC
+#     ORDER BY M.CODFILIAL
+#         , M.DTMOV DESC
+#         , M.NUMNOTA DESC
+# """
 
 
-print("Tentando conectar ao banco de dados Oracle...")
-try:
-    with oracledb.connect(user=user, password=password, dsn=dsn) as connection:
-        print("Conexão bem-sucedida. Executando consultas...")
+# print("Tentando conectar ao banco de dados Oracle...")
+# try:
+#     with oracledb.connect(user=user, password=password, dsn=dsn) as connection:
+#         print("Conexão bem-sucedida. Executando consultas...")
 
-        # 1. Carregar Dados
-        df_inventario_pendete_baixa = pd.read_sql(sql_inventario_pendente_baixa, con=connection)
+#         # 1. Carregar Dados
+#         df_inventario_pendete_baixa = pd.read_sql(sql_inventario_pendente_baixa, con=connection)
 
-        NOME_ARQUIVO = 'acompanhamento_inventario_pendente_baixa.csv'
+#         NOME_ARQUIVO = 'acompanhamento_inventario_pendente_baixa.csv'
 
-        df_inventario_pendete_baixa.to_csv(
-            NOME_ARQUIVO, 
-            index=False, 
-            sep=';', 
-            encoding='utf-8-sig',
-            decimal=',' 
-        )
+#         df_inventario_pendete_baixa.to_csv(
+#             NOME_ARQUIVO, 
+#             index=False, 
+#             sep=';', 
+#             encoding='utf-8-sig',
+#             decimal=',' 
+#         )
 
-        print("\nTabela final de Acompanhamento de Inventário criada e salva como 'acompanhamento_inventario_detalhado.csv'! ✅")
+#         print("\nTabela final de Acompanhamento de Inventário criada e salva como 'acompanhamento_inventario_detalhado.csv'! ✅")
 
-except oracledb.Error as e:
-    error_obj, = e.args
-    print(f"Erro ao se conectar com o banco Oracle: ORA-{error_obj.code}: {error_obj.message}")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
+# except oracledb.Error as e:
+#     error_obj, = e.args
+#     print(f"Erro ao se conectar com o banco Oracle: ORA-{error_obj.code}: {error_obj.message}")
+# except Exception as e:
+#     print(f"Ocorreu um erro: {e}")
